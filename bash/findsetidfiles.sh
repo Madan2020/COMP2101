9e4f6c6dfed8e9ee268be0cbfcdfb34a6e992a82
Madan Neupane <200418778@student.georgianc.on.ca>
2020-06-25 21:34:32 -0400

    lab #03 Assignment

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -19,18 +19,73 @@
 # use the find command to generate the list of files with their sizes, with an error redirect to /dev/null
 # use cut or awk to display only the output desired
 
+# echo command in linux scripting displays/print the written text inside the duoble quotation. It is used to display a line of a text or a string on standard output which is passed as a arguments.
+# 
 echo "Setuid files:"
 echo "============="
 find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3
 echo ""
 
-# Displaying setgid files
+# here in task 1 the script is modified in such a way that it  finds and display the setgid files in a second listing.
+
+# Displaying setgid files in the system.
+
+# echo command in linux scripting displays/print the written text inside the duoble quotation.
+
+# here setgid is the abbreviation for "Set Group ID", it allows me to run an executable with the permissions and to change behaviour in directories.
+
 echo "Setgid files:"
 echo "=============="
+
+# Display files with permissions set to 2000.
+
+# here find command is used to searching the particular files.
+
+# / gives information about our directories either home or root directory.
+
+# 2> /dev/null redirects to /dev/null if any error is encountered in the scripts.
+
+# sort -k is used to sort file on basis of key.
+
+# here "|" is called pipe. | pipe is used to combine two or more commands in linux scriping.
+
+#echo command in linux scripting displays/print the written text which is inside the duoble quotation.
+
 find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 4
 echo ""
 
-# Displaying latest files
-echo "Largest files:"
+# here in task 2 the script is modified in such a way that it displays ten  largest files in the system.
+
+# here echo command in linux scripting displays/print the written text which is inside the duoble quotation.
+
+echo "10 Largest files in the system:"
 echo "============="
-find / -type f -exec du -Sh {} + 2>/dev/null | sort -rh | head -n 10
+find / 2>/dev/null -type f -exec ls -l --block-size=M {} + | sort -rh -k 5 | head -n 10 | awk '{print $5, $3, $9}'
+
+# here find command is used to search the specific files.
+
+# 2> /dev/null redirects to /dev/null if error is encountered.
+
+# here du command is used to estimate disk usages.
+
+# / gives information about our directories either home or root directory.
+
+# du -sh compares human readable numbers and shows total of for each argument.
+
+# sort -rh sorts value in human readable format and reverse results.
+
+# {} are used to summarize disk usage of each file.
+
+# here "|" is called pipe. | pipe is used to combine two or more commands in linux scriping.
+
+# here in linux scripting head command returns the first ten lines of each file name that is provided to it. head -n 10 displays first 10 files here.
+
+ls -l --block-size=M / 2>/dev/null | sort -h -r | head -n 10
+du --block-size=M / 2>/dev/null | sort -h -r | head -n 10
+du -ha | sort -h -r | head -n 10
+
+# find -type f -exec ls -l --block-size=M {} + | sort -rh -k 5 | head -n 10 | awk '{print $5, $3, $9}'
+
+# here M indicates size in Mbytes.
+
+# here awk command  manipulates and generates reports.
---
+++ bash/lab 03/passwordguesser.sh
@@ -0,0 +1,62 @@
+# This script demonstrates testing to see if 2 strings are the same
+# it also demonstrates using conditional execution
+
+# TASK 1: Improve it by asking the user for a password guess instead of using inline literal data (i.e. TestString)
+# TASK 2: Improve it by rewriting it to use the if command
+#         The if command should test if they got it right, in which case it should tell them they got it right
+#         The if command should tell them they got it wrong
+# TASK 3: Improve it by giving them 5 tries to get it right before failing
+#           *** Do not use the exit command, and do not use a loop (e.g. while or for)
+
+myString="TestString"
+referenceString="password"
+
+[ $myString = $referenceString ] && echo "Correct!" || echo "Incorrect."
+
+echo "Enter random guessing password"
+read myString
+referenceString="password"
+if [ $myString = $referenceString ]
+then
+  echo ""
+  echo "Yes! Your entered password is correct"
+  echo ""
+else
+  echo "You have entered incorrect password. Please try again"
+  read myString1
+  if [ $myString1 = $referenceString ]
+  then
+    echo ""
+    echo "Yes! Your entered password is correct"
+    echo ""
+  else
+    echo "You have entered incorrect password. Please try again"
+    read myString2
+    if [ $myString2 = $referenceString ]
+    then
+      echo ""
+      echo "Yes! Your entered password is correct"
+      echo ""
+    else
+      echo "You have entered incorrect password. Please try again"
+      read myString3
+      if [ $myString3 = $referenceString ]
+      then
+        echo ""
+        echo "Yes! Your entered password is correct"
+        echo ""
+      else
+        echo "You have entered incorrect password. Please try again"
+        read myString4
+        if [ $myString4 = $referenceString ]
+        then
+          echo ""
+          echo "Yes! Your entered password is correct"
+          echo ""
+        else
+          echo "You have enetered incorrect password five times. please try again later"
+        fi
+      fi
+    fi
+  fi
+fi
