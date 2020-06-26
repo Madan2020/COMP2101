ec5451d7e751acaada28416f23c1edb83a076214
Madan2020 <65733092+Madan2020@users.noreply.github.com>
2020-06-25 23:39:29 -0400

    lab 2 updates

--- bash/diceroller.sh
+++ bash/diceroller.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
 #
-# this script rolls a pair of six-sided dice
+# this script rolls a six-sided die
 #
 
-# Improve this script by adding a second line of output showing a roll of 5 six-sided dice
-# Improve this script by adding a third line of output that rolls a single 20-sided die
-# These improvements should also take into consideration the user, and make all 3 output lines self-explanatory to the user
-#    - e.g. the first line should be changed to read something like:
-#         Rolling a pair of standard dice: x,y rolled
-#      and the other output lines should be similarly helpful to the user
+# Task: Improve this script by making it roll as many dice as there are letters in your first name, instead of just one
 
 # roll the dice and display the result
-echo "
-Rolling a pair of standrard dice : 6 side rolled
-Now it's Rolling...
-$(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1 )) rolled
-"
-echo "
-Rolling a pair of standrard dice : 5,6 side rolled
-Now it's Rolling...
-$(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1 )), $(( RANDOM % 6 + 1 )), $(( RANDOM % 6 + 1 )), $(( RANDOM % 6 + 1 )) rolled
-"
 
+echo "Enter your first name :"
+read name
+length=${#name}
+for ((c=1; c<=$length; c++))
+do
 echo "
-Rolling a pair of standrard dice : 20 side rolled
-Now it's Rolling...
-$(( RANDOM % 20 + 1)), $(( RANDOM % 20 + 1 )) rolled
+Rolling...
+$(( RANDOM % 6 + 1)) rolled
 "
+done
