a53b1fab2006ae6016742db79460948fdf0206dd
Madan2020 <65733092+Madan2020@users.noreply.github.com>
2020-06-26 00:01:28 -0400

    Lab 02 updates

--- bash/getpics.sh
+++ bash/getpics.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #
 # this script puts some picture files into a Pictures directory in our home directory
-# it only runs commands if they need to be run and only shows necessary output
+# it does some error testing
 # it summarizes the Pictures directory when it is done
 #
 
-# Improve this script to also retrieve and install the files kept in the https://zonzorp.net/pics.tgz tarfile
-#   - use the same kind of testing to make sure commands work and delete the local copy of the tarfile when you are done with it
+# Task 1: Improve this script to also retrieve and install the files kept in the https://zonzorp.net/pics.tgz tarfile
+#   - use the same kind of testing that is already in the script to only download the tarfile if you don't already have it and  to make sure the download and extraction commands work, then delete the local copy of the tarfile if the extraction was successful
 
 # make a Pictures directory if we don't have one - assumes we have a home directory
 test -d ~/Pictures || mkdir ~/Pictures
+
+# || only runs second command if first commands fails. so if Picture directory doesn't already exist in home directory it creates a Pictures directory
 
 # download a zipfile of pictures to our Pictures directory if it isn't already there - assumes you are online
 test -f ~/Pictures/pics.zip || wget -q -O ~/Pictures/pics.zip http://zonzorp.net/pics.zip
@@ -17,14 +19,22 @@ test -f ~/Pictures/pics.zip || wget -q -O ~/Pictures/pics.zip http://zonzorp.net
 # unpack the downloaded zipfile if it is there, then delete the local copy of the zipfile
 test -f ~/Pictures/pics.zip && unzip -d ~/Pictures -o -q ~/Pictures/pics.zip && rm ~/Pictures/pics.zip
 
-#make a pictures directory if we don't have one - assumes we have a home directory
-test -f ~/Pictures/pics.tgz || wget -q -O ~/Pictures/pics.tgz http://zonzorp.net/pics.tgz
-
-#download a zipfile of pictures to our directory if it isn't already there - assumes you are online
-
-test -f ~/Pictures/pics.tgz && tar -C ~/Pictures -xvf ~/Pictures/pics.tgz && rm ~/Pictures/pics.tgz
-
 # Make a report on what we have in the Pictures directory
+test -d ~/Pictures && cat <<EOF
+Found $(find ~/Pictures -type f|wc -l) files in the Pictures directory.
+The Pictures directory uses $(du -sh ~/Pictures|awk '{print $1}') space on the disk.
+EOF
+
+#downloading pics.tgztarfile
+
+test -f ~/Pictures/pics.tar || wget -q -O ~/Pictures/pics.tar http://zonzorp.net/pics.tgz
+
+test -f ~/Pictures/pics.tar && tar -xvf ~/Pictures/pics.tar -C ~/Pictures/ && rm ~/Pictures/pics.tar
+
+# here tar -xvf is used to extract .tar extensions.
+
+# -C specifies in which directory does file needs to be extracted
+
 test -d ~/Pictures && cat <<EOF
 Found $(find ~/Pictures -type f|wc -l) files in the Pictures directory.
 The Pictures directory uses $(du -sh ~/Pictures|awk '{print $1}') space on the disk.
